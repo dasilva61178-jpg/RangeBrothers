e820b21f72708994c694f7abee39c1883a93fdc6
Dasil <dasil@example.com>
2026-02-11 00:29:34 -0500

    add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,13 @@
+
+export default function sitemap() {
+  return [
+    {
+      url: "https://rangebrothers.store",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://rangebrothers.store/products",
+      lastModified: new Date(),
+    },
+  ];
+}
